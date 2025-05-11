00799096f7355d54c3cc6fab832e0a1a8e2b8929
Muhammed Özdal <mozdal13@gmail.com>
2025-05-11 16:56:51 +0200

    legg_til_bil funksjon

--- prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
+++ prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
@@ -46,12 +46,39 @@ public:
 		m_person_nr{ person_nr }, m_navn{ navn } {};
 
 	void legg_til_kjoretoy(std::shared_ptr<kjoretoy> kjrt) {
-
+		m_kjoretoy.push_back(kjrt);
 
 	}
+	std::string get_person_nr(){
+		return m_person_nr;
 
+	}
+	friend person;
 };
 
+class unntak : public std::exception {}; // oppgave 7
+
+class kjoretoy_register {
+private:
+	std::vector<std::shared_ptr<person>> m_eiere;
+
+public:
+	void legg_til_person(std::string person_nr, std::string navn) {
+		std::shared_ptr<person> eier = std::make_shared<person>(person_nr, navn);
+		m_eiere.push_back(eier);
+	}
+
+	void legg_til_bil(std::string person_nr, std::string merke, std::string modell, std::string antall_seter) {
+		std::shared_ptr<person> eier;
+		for (auto it = m_eiere.begin(); it != m_eiere.end(); it++) {
+			if ((*it)->get_person_nr() == person_nr) {
+				std::shared_ptr<bil> eier  = std::make_shared<bil>(person_nr, merke, modell, antall_seter);
+				eier->legg_til_kjoretoy(bil);
+			}
+		}
+
+	}
+};
 
 
 
