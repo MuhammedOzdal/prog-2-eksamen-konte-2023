e0be39d9c1891d0c235fb366c74fe3faae4158d4
Muhammed Özdal <mozdal13@gmail.com>
2025-05-11 14:11:46 +0200

    test

--- prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
+++ prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
@@ -6,14 +6,32 @@ private:
 	std::string m_reg_nr, m_merke, m_modell, m_eier;
 
 public:
+	kjoretoy(std::string reg_nr, std::string merke, std::string modell, std::string eier) :
+		m_reg_nr{ reg_nr }, m_merke{ merke }, m_modell{ modell }, m_eier{ eier } {};
+	
+	virtual void f() = 0;
+};
 
+class bil : public kjoretoy {
+private:
+	unsigned int m_antall_seter;
+
+public:
+	
 
 };
 
 
 
 
-int main()
-{
 
+
+
+
+
+int main() {
+
+
+
+	return 0;
 }
