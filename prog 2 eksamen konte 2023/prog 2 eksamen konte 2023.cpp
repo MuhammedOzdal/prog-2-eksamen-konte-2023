a37cc544d3e48e39eb1bc460b6de61b437b8988c
Muhammed Özdal <mozdal13@gmail.com>
2025-05-11 15:49:44 +0200

    .

--- prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
+++ prog 2 eksamen konte 2023/prog 2 eksamen konte 2023.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class kjoretoy {
 private:
@@ -17,10 +18,39 @@ private:
 	unsigned int m_antall_seter;
 
 public:
+	bil(std::string reg_nr, std::string merke, std::string modell, std::string eier, unsigned int antall_seter) :
+		kjoretoy{ reg_nr, merke, modell, eier }, m_antall_seter{ antall_seter } {};
 	
-
+	virtual void f() {};
 };
 
+class motorsykkel : public kjoretoy {
+private:
+	bool m_sidevogn;
+
+public:
+	motorsykkel(std::string reg_nr, std::string merke, std::string modell, std::string eier, bool sidevogn) :
+		kjoretoy{ reg_nr, merke, modell, eier }, m_sidevogn{ sidevogn } {};
+
+	virtual void f() {};
+};
+
+class person {
+private:
+	std::string m_person_nr;
+	std::string m_navn;
+	std::vector<std::shared_ptr<kjoretoy>> m_kjoretoy;
+
+public:
+	person(std::string person_nr, std::string navn) :
+		m_person_nr{ person_nr }, m_navn{ navn } {};
+
+	void legg_til_kjoretoy(std::shared_ptr<kjoretoy> kjrt) {
+
+
+	}
+
+};
 
 
 
